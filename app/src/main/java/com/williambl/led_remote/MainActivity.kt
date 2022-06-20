5ba8f788ae53a997754bee8f3261a5ab7152a0b1
williambl <william@williambl.com>
2022-06-20 20:31:25 +0100

    Initial Commit

---
+++ app/src/main/java/com/williambl/led_remote/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.williambl.led_remote
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
